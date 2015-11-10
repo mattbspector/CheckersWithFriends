4acc7b07a8c3cdd30f67e31c3f5f1a9269b16488
Justin Platz <jplatz@umich.edu>
2015-11-10 14:38:41 -0500

    Board Setup, Edit Board String

--- js/board.js
+++ js/board.js
@@ -1,10 +1,19 @@
-    
+//DEFINE NEW_BOARD_STRING = "101000301000303010100030100030301010003010003030101000301000303"  
+var boardString = '{"board" : "1010003001000303101000300100030310100030010003031010003001000303"}';
 $(window).ready(function() {
+      
+    
+      //Query History and set Board String
+    
+    
       //Setup the board and choose the starting color
       setup('red');
       $("#turn_display").html("Gray's Turn");
     });
 
+    
+    
+    
 
     function setup(startingColor){
       //Lay down the checkerboard
@@ -16,14 +25,27 @@ $(window).ready(function() {
           $('.checkerBoard tr[row='+i+']').append('<td class="cell '+color+'" row="'+i+'" col="'+j+'"></td>');
         }
       }
-      //Create the pieces
-      for (var i=0;i<4;i++){
-        for (var j=0;j<3;j++){
-          $('.checkerBoard [row='+(i*2+(j%2))+'][col='+j+']').append('<div class="piece black"><div class="kingCenter">&#9812;</div></div>');
-          $('.checkerBoard [row='+(i*2+((j+1)%2))+'][col='+(7-j)+']').append('<div class="piece red"><div class="kingCenter">&#9812;</div></div>');
+      var boardJson = JSON.parse(boardString);
+                    var board = boardJson["board"];
+
+      //Create the pieces for a new game
+        //"101000301000303010100030100030301010003010003030101000301000303"  
+      for (var i=0;i<8;i++){
+        for (var j=0;j<8;j++){
+            if(board[(8 * i) + j] == 1){
+                 $('.checkerBoard [row='+i+'][col='+j+']').append('<div class="piece black"><div class="kingCenter">&#9812;</div></div>');
+            }
+            else if(board[(8 * i) + j] == 2){
+                 $('.checkerBoard [row='+i+'][col='+j+']').append('<div class="piece black king"><div class="kingCenter">&#9812;</div></div>');
+            }
+            else if(board[(8 * i) + j] == 3){
+                 $('.checkerBoard [row='+i+'][col='+j+']').append('<div class="piece red"><div class="kingCenter">&#9812;</div></div>');
+            }
+            else if(board[(8 * i) + j] == 4){
+                 $('.checkerBoard [row='+i+'][col='+j+']').append('<div class="piece black king"><div class="kingCenter">&#9812;</div></div>');
+            }
         }
       }
-
       //give the pieces draggablility
       $('.piece').draggable({
           containment: '.checkerBoard',
@@ -78,12 +100,45 @@ $(window).ready(function() {
       });
     }
 
+    function changJsonString(piece, oldcol, oldrow, newcol, newrow){
+        var newindex = (4 * newrow) + newcol;
+        var oldindex = (4 * oldrow) + oldcol;
+        var boardJson = JSON.parse(boardString);
+        boardJson["board"][oldindex] = 0;
+        if(piece.hasClass('red')){
+            if(piece.hasClass('king')){
+                boardJson["board"][newindex] = 4;
+
+            }
+            else{
+                boardJson["board"][newindex] = 3;
+
+            }
+        }
+        else{
+            if(piece.hasClass('king')){
+                boardJson["board"][newindex] = 2;
+
+            }
+            else{
+                boardJson["board"][newindex] = 1;
+            }
+        }
+        if (Math.abs(oldrow-newrow) == 2 || Math.abs(oldcol-newcol) == 2){
+            var middleRow = (oldrow + newrow)/2;
+            var middleCol = (oldcol + newcol)/2;
+            var middleIndex = (4 * middleRow) + middleCol;
+            boardJson["board"][middleIndex] = 0;
+        }
+        
+        //Publish boardJSON
+    }
+        
     function handlePieceDrop(event, ui){
       var oldRow = parseInt(ui.draggable.parent().attr('row'));
       var oldCol = parseInt(ui.draggable.parent().attr('col'));
       var newCol = parseInt($(this).attr('col'));
       var newRow = parseInt($(this).attr('row'));
-
       removeActive();
       $(this).append(ui.draggable);
       $('#draggableHelper').remove();
@@ -101,12 +156,14 @@ $(window).ready(function() {
         if (ui.draggable.hasClass('red')){
           $('.piece.red').draggable('disable');
           $('.piece.black').draggable('enable');
+          ///
           $("#turn_display").html("Gray's Turn");
 
         }
         else{
           $('.piece.black').draggable('disable');
           $('.piece.red').draggable('enable');
+          ///
           $("#turn_display").html("Red's Turn");
 
         }
@@ -115,6 +172,8 @@ $(window).ready(function() {
           $('.piece').draggable('disable');
           ui.draggable.draggable('enable');
       }
+        
+      changJsonString(ui.draggable, oldCol, oldRow, newCol, newRow);
     }
 
     function jumpPiece(piece,color,oldRow,oldCol,newRow,newCol){
@@ -132,6 +191,7 @@ $(window).ready(function() {
     function myHelper( event ) {return '<div id="draggableHelper" class="piece"></div>';}
 
     function victory(color){
+      //Publish : boardString = '{"board" : "101000301000303010100030100030301010003010003030101000301000303"}'
       if (color == 'black'){$('.victory').html('Black Winnnnnnssssss!!!!!');}
       else{$('.victory').html('Red Winnnnnnssssss!!!!!');}
 
